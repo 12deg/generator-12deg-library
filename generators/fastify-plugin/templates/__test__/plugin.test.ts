a7a80c17d36a57b3b4464e87d14ec22e60ddcbfc
Dipendra Upreti <dipendra@dzangolab.com>
2024-08-21 15:24:11 +0545

    chore: update template

--- generators/fastify-plugin/templates/__test__/plugin.test.ts
+++ generators/fastify-plugin/templates/__test__/plugin.test.ts
@@ -1,12 +1,12 @@
 import Fastify from "fastify";
 import { describe, it, expect } from "vitest";
 
-import <%= scope %>Plugin from "../src/index";
+import <%= baseName %>Plugin from "../src/index";
 
-describe("<%= scope %> plugin", () => {
+describe("<%= baseName %> plugin", () => {
   it("should be exported as default and register without issues", async () => {
     const fastify = Fastify();
 
-    await expect(fastify.register(<%= scope %>Plugin)).resolves.not.toThrow();
+    await expect(fastify.register(<%= baseName %>Plugin)).resolves.not.toThrow();
   });
 });
