86379ef16ef189aa5cd952188d7ab533bc56fe5f
Dipendra Upreti <dipendra@dzangolab.com>
2024-10-24 14:22:22 +0545

    chore: fix lint issue

--- generators/fastify-plugin/templates/src/plugin.ts
+++ generators/fastify-plugin/templates/src/plugin.ts
@@ -5,10 +5,11 @@ import type { FastifyInstance } from "fastify";
 const plugin = FastifyPlugin(
   async (
     fastify: FastifyInstance,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     options: Record<never, never>,
   ) => {
     fastify.log.info("Registering <%= name %> plugin");
-  }
+  },
 );
 
 export default plugin;
